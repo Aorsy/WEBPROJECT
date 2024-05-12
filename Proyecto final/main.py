b72fad78e1199c72c496da3d85aa0171fbdf03f1
Aorsy <juanchowd420@gmail.com>
2024-05-11 22:20:06 -0500

    Added three endpoint and connected an endpoint to an API

--- Proyecto final/main.py
+++ Proyecto final/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 from flask_mysqldb import MySQL
+import requests
 
 app = Flask(__name__)
 
@@ -16,6 +17,26 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/api',methods=['GET','POST'])
+def api():
+    if request.method == 'GET':
+        return render_template('api.html')
+  #Si se recibe por POST
+    if request.form['search']:
+        url = "https://www.datos.gov.co/resource/w735-yw39.json?nombre=" +request.form['search']
+    #Obtener la información y almacenarla en una variable
+        giphy = requests.get(url)
+    #Obtener los datos en formato JSON
+        dataG = giphy.json()
+    #Renderizado de la data
+        return render_template('api.html', data=dataG)
+    else:
+        return render_template('api.html')
 
 
 if __name__ == '__main__':
